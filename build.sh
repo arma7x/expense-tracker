c0d73a7d8bfc1cdaef4d9cfe7414c1ef0d06c2ed
Ahmad Malik <arma7x@live.com>
2023-04-30 14:26:54 +0800

    1st commit

---
+++ build.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+REV=$(($(date +%s%N)/1000000))
+ZIP_BUILD="build"
+
+rm ./*.zip 2> /dev/null
+rm ./public/build/* 2> /dev/null
+yarn build
+rm ./public/build/*.map
+cd ./public
+
+kaios=(2.5 3.0)
+exc_manifest=(manifest.webmanifest manifest.webapp)
+
+for (( i=0;i<${#kaios[@]};i++ ))
+do
+  file_name="${ZIP_BUILD}_${kaios[$i]}_${REV}.zip"
+  zip -r "./${file_name}" * -x "${exc_manifest[$i]}" && mv "./${file_name}" ../
+done
