c7305bbdcb83d65d01672ca9627db49b6c475703
Ahmad Malik <arma7x@live.com>
2023-05-03 13:40:19 +0800

    remove serve()

--- rollup.config.js
+++ rollup.config.js
@@ -217,7 +217,7 @@ export default [
 
       // In dev mode, call `npm run start` once
       // the bundle has been generated
-      !production && serve(),
+      !production && (function(){})(),
 
       // Watch the `public` directory and refresh the
       // browser on changes when not in production
